297a589f246a93a44b04968f78f6a798244420a5
Junze Bao <junze@poolside.ai>
2025-08-28 16:05:50 +0200

    add resource name ignore
    Signed-off-by: Junze Bao <junze@poolside.ai>

--- pkg/scheduler/api/resource_info.go
+++ pkg/scheduler/api/resource_info.go
@@ -415,6 +415,9 @@ func (r *Resource) LessEqual(rr *Resource, defaultValue DimensionDefaultValue) b
 
 	if defaultValue == Infinity {
 		for name := range rr.ScalarResources {
+			if IgnoreScalarResource(name) {
+				continue
+			}
 			if _, ok := r.ScalarResources[name]; !ok {
 				return false
 			}
@@ -422,6 +425,9 @@ func (r *Resource) LessEqual(rr *Resource, defaultValue DimensionDefaultValue) b
 	}
 
 	for resourceName, leftValue := range r.ScalarResources {
+		if IgnoreScalarResource(resourceName) {
+			continue
+		}
 		rightValue, ok := rr.ScalarResources[resourceName]
 		if !ok && defaultValue == Infinity {
 			continue
@@ -461,7 +467,7 @@ func (r *Resource) LessEqualWithDimension(rr *Resource, req *Resource) bool {
 	}
 
 	for name, quant := range req.ScalarResources {
-		if IsIgnoredScalarResource(name) {
+		if IgnoreScalarResource(name) {
 			continue
 		}
 		rQuant := r.ScalarResources[name]
@@ -789,4 +795,8 @@ func ExceededPart(left, right *Resource) *Resource {
 
 	diff, _ := left.Diff(right, Zero)
 	return diff
+}
+
+func IgnoreScalarResource(name v1.ResourceName) bool {
+	return name == "attachable-volumes-csi-fsx.csi.aws.com" || name == "efa.poolsi.de/infiniband" || name == "vpc.amazonaws.com/efa" || ignoredScalarResources.Has(string(name))
 }
